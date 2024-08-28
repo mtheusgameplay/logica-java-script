df88ce14230808337244f1c93de97e983387cd1b
mtheusgameplay <163136497+mtheusgameplay@users.noreply.github.com>
2024-08-28 07:58:14 -0300

    java scrr

---
+++ index.js
@@ -0,0 +1,32 @@
+// Usando var
+var nome = "John";
+
+// Usando let
+let idade = 25;
+
+// Usando const
+const pi = 3.14;
+
+let a = 5;
+let b = 10;
+let soma = a + b; // Armazena o resultado da soma de a e b na variável sum console.log (soma); // 15
+
+let a = 5;
+let b = 10;
+
+function multiplica (x,y) {
+    return x * y;
+}
+
+let produto = multiplica (a, b); // Armazena o resultado da função multiplica na console.log (produto); // 50
+
+let frutas = ['aple', 'banana', 'cherry'];
+console.log (frutas[1]); // banana
+
+let pessoa = {
+    primeiroNome: 'John',
+    sobrenome: 'Delon',
+    age: 25
+};
+
+console.log(pessoa.primeiroNome); // John
